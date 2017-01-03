1e6b4b196e084f80ef7539123453759f0fd9c1ff
istreeter <streeter@ebi.ac.uk>
2017-01-03 11:39:59 +0000

    Script for syncing elasticsearch from staging to production

---
+++ shell/elasticsearch_snapshot.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This script must be run as user reseq_adm because it involves rsyncing from hx to pg
+
+perl $GCA_ELASTICSEARCH/scripts/sync_hx_hh.es.pl \
+  -from ves-hx-e4 \
+  -to ves-pg-e4 \
+  -to ves-oy-e4 \
+  -repo faang_repo \
+  -snap_index faang
