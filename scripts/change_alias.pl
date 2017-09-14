7738c3269aad81e858a5b75a044253b8dfba30c7
Jun Fan <junf@ebi.ac.uk>
2017-09-14 15:41:10 +0100

    add function to print current alias and specify the alias to change

--- scripts/change_alias.pl
+++ scripts/change_alias.pl
@@ -15,14 +15,15 @@ my $faang_alias = "faang";
 GetOptions(
   'es_host=s' =>\$es_host,
   'es_index=s' =>\$es_index,
+  'es_alias=s' =>\$faang_alias,
 );
-croak "Need -es_index e.g. faang_build_1 which will be the new target index of the alias" unless ( $es_index);
+croak "Need -es_index e.g. faang_build_1 which will be the new target index of the alias or the specific value of CURRENT to print current alias" unless ( $es_index);
 
 my $term;
 open $term, "curl -XGET \'$es_host/_alias\'|";
 my $json = decode_json(&readHandleIntoString($term));
 my %current = %$json;
-die "Could not find the specified index $es_index at server $es_host" unless (exists $current{$es_index});
+die "Could not find the specified index $es_index at server $es_host" unless (exists $current{$es_index} || $es_index eq "CURRENT");
 my %in_use;
 foreach my $name(keys %current){
 	my %alias = %{$current{$name}{aliases}};
@@ -31,6 +32,14 @@ foreach my $name(keys %current){
 		$in_use{$name} = 1 if ($alias eq $faang_alias);
 	}
 }
+
+if ($es_index eq "CURRENT"){
+	my @in_use = sort {$a cmp $b} keys %in_use;
+	my $in_use = join (",",@in_use);
+	print "The current alias is $in_use\n";
+	exit;
+}
+
 if (exists $in_use{$es_index}){
 	print "Index $es_index is already assigned with the alias $faang_alias\n";
 	exit;
