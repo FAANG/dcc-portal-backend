fe3d23b84bd801b709a6da4c886dc8aaaa4a09c7
Alexey Sokolov <alexey@ebi.ac.uk>
2018-11-16 11:30:57 +0000

    Adding script to snapshot creating

---
+++ scripts/sync_hx_hh.es.py
@@ -0,0 +1,84 @@
+import os
+from elasticsearch import Elasticsearch
+from datetime import date, timedelta
+
+# Addresses of servers
+STAGING_NODE1 = 'wp-np3-e2:9200'
+STAGING_NODE2 = 'wp-np3-e3:9200'
+FALLBACK_NODE1 = 'wp-p2m-e2:9200'
+FALLBACK_NODE2 = 'wp-p2m-e3:9200'
+PRODUCTION_NODE1 = 'wp-p1m-e2:9200'
+PRODUCTION_NODE2 = 'wp-p1m-e3:9200'
+
+# Paths for rsync command
+FROM = '/nfs/public/rw/reseq-info/elastic_search_staging/snapshot_repo/es6_faang_repo/'
+TO = '/nfs/public/rw/reseq-info/elastic_search/snapshot_repo/es6_faang_repo/'
+
+
+def main():
+    # Define connection to 3 nodes
+    es_staging = Elasticsearch([STAGING_NODE1, STAGING_NODE2])
+    es_fallback = Elasticsearch([FALLBACK_NODE1, FALLBACK_NODE2])
+    es_production = Elasticsearch([PRODUCTION_NODE1, PRODUCTION_NODE2])
+
+    # Get current date and snapshot name
+    today = date.today().strftime('%Y-%m-%d')
+    yesterday = (date.today() - timedelta(1)).strftime('%Y-%m-%d')
+    snapshot_name = "snapshot_{}".format(today)
+
+    # Do all the job
+    create_snapshot(es_staging, snapshot_name, today, snapshot_name)
+    rsync_snapshot()
+    restore_snapshot(es_fallback, es_production)
+    change_aliases(es_fallback, es_production, today, yesterday)
+    delete_old_indices(es_fallback, es_production, yesterday)
+
+
+def create_snapshot(es_staging, snapshot_name):
+    parameters = {"indices": "faang1",
+                  "ignore_unavailable": True,
+                  "include_global_state": False
+                  }
+    es_staging.snapshot.create(repository='es6_faang_repo', snapshot=snapshot_name, body=parameters)
+
+
+def rsync_snapshot():
+    os.system("rsync --archive --delete-during {} {}".format(FROM, TO))
+
+
+def restore_snapshot(es_fallback, es_production, today, snapshot_name):
+    parameters = {
+        "indices": "file,organism,specimen,dataset",
+        "ignore_unavailable": True,
+        "rename_pattern": "([a-z]+)",
+        "rename_replacement": "$1-{}".format(today)
+    }
+    es_fallback.snapshot.restore(repository='es6_faang_repo', snapshot=snapshot_name, body=parameters)
+    es_production.snapshot.restore(repository='es6_faang_repo', snapshot=snapshot_name, body=parameters)
+
+
+def change_aliases(es_fallback, es_production, today, yesterday):
+    actions = {"actions": [
+        {"remove": {"index": "file-{}".format(yesterday), "alias": "file"}},
+        {"add": {"index": "file-{}".format(today), "alias": "file"}},
+        {"remove": {"index": "organism-{}".format(yesterday), "alias": "organism"}},
+        {"add": {"index": "organism-{}".format(today), "alias": "organism"}},
+        {"remove": {"index": "specimen-{}".format(yesterday), "alias": "specimen"}},
+        {"add": {"index": "specimen-{}".format(today), "alias": "specimen"}},
+        {"remove": {"index": "dataset-{}".format(yesterday), "alias": "dataset"}},
+        {"add": {"index": "dataset-{}".format(today), "alias": "dataset"}},
+    ]
+    }
+    es_fallback.indices.update_aliases(body=actions)
+    es_production.indices.update_aliases(body=actions)
+
+
+def delete_old_indices(es_fallback, es_production, yesterday):
+    es_fallback.indices.delete(index="file-{},organism-{},specimen-{},dataset-{}").format(yesterday, yesterday,
+                                                                                          yesterday, yesterday)
+    es_production.indices.delete(index="file-{},organism-{},specimen-{},dataset-{}").format(yesterday, yesterday,
+                                                                                            yesterday, yesterday)
+
+
+if __name__ == "__main__":
+    main()
