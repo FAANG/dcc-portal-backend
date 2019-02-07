f7b80664b46ff0a5260738a61126af2e9475b856
Jun Fan <junf@ebi.ac.uk>
2019-02-07 09:04:54 +0000

    add etag cache file clean up script

---
+++ scripts/clean_etag_cache.pl
@@ -0,0 +1,12 @@
+use strict;
+
+my $copy_cache = 2;
+open CMD, "ls -tl etag_list_*.txt|";
+my @records = <CMD>;
+for (my $i = $copy_cache; $i < scalar @records; $i++){
+	if ($records[$i]=~/(etag_list_\d+-\d+-\d+\.txt)\s*$/){
+		my $cmd = "rm $1";
+#		print "$cmd\n";
+		system($cmd);
+	}
+}
