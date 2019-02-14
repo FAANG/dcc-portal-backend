30320d9a62b70fc89f6cf7103a52aa5d59012edc
Alexey Sokolov <alexey@ebi.ac.uk>
2019-02-14 10:04:24 +0000

    Fixing bug with sync script

--- scripts/sync_hx_hh.es.py
+++ scripts/sync_hx_hh.es.py
@@ -2,6 +2,7 @@ import os
 from elasticsearch import Elasticsearch
 from datetime import date, timedelta
 from multiprocessing import Process
+import time
 
 # Addresses of servers
 STAGING_NODE1 = 'wp-np3-e2:9200'
@@ -44,6 +45,7 @@ def main():
     for process in processes:
         process.start()
         process.join()
+        time.sleep(5)
 
 
 def create_snapshot(es_staging, snapshot_name):
@@ -86,10 +88,10 @@ def change_aliases(es_fallback, es_production, today, yesterday):
         {"add": {"index": "dataset-{}3".format(today), "alias": "dataset"}},
         {"remove": {"index": "experiment-{}3".format(yesterday), "alias": "experiment"}},
         {"add": {"index": "experiment-{}3".format(today), "alias": "experiment"}},
-        {"remove": {"index": "protocol_files-{}3".format(yesterday), "alias": "protocol_files"}},
-        {"add": {"index": "protocol_files-{}3".format(today), "alias": "protocol_files"}},
-        {"remove": {"index": "protocol_samples-{}3".format(yesterday), "alias": "protocol_samples"}},
-        {"add": {"index": "protocol_samples-{}3".format(today), "alias": "protocol_samples"}},
+        {"remove": {"index": "protocol-{}_files-{}3".format(yesterday, yesterday), "alias": "protocol_files"}},
+        {"add": {"index": "protocol-{}_files-{}3".format(today, today), "alias": "protocol_files"}},
+        {"remove": {"index": "protocol-{}_samples-{}3".format(yesterday, yesterday), "alias": "protocol_samples"}},
+        {"add": {"index": "protocol-{}_samples-{}3".format(today, today), "alias": "protocol_samples"}},
     ]
     }
     es_fallback.indices.update_aliases(body=actions)
@@ -98,8 +100,8 @@ def change_aliases(es_fallback, es_production, today, yesterday):
 
 def delete_old_indices(es_fallback, es_production, yesterday):
     print("Deleting old indices...")
-    es_fallback.indices.delete(index="file-{}3,organism-{}3,specimen-{}3,dataset-{}3,experiment-{}3,protocol_files-{}3,protocol_samples-{}3".format(yesterday, yesterday, yesterday, yesterday, yesterday))
-    es_production.indices.delete(index="file-{}3,organism-{}3,specimen-{}3,dataset-{}3,experiment-{}3,protocol_files-{}3,protocol_samples-{}3".format(yesterday, yesterday, yesterday, yesterday, yesterday))
+    es_fallback.indices.delete(index="file-{}3,organism-{}3,specimen-{}3,dataset-{}3,experiment-{}3,protocol-{}_files-{}3,protocol-{}_samples-{}3".format(yesterday, yesterday, yesterday, yesterday, yesterday))
+    es_production.indices.delete(index="file-{}3,organism-{}3,specimen-{}3,dataset-{}3,experiment-{}3,protocol-{}_files-{}3,protocol-{}_samples-{}3".format(yesterday, yesterday, yesterday, yesterday, yesterday))
 
 
 if __name__ == "__main__":
