fdc124e089e553dfcaf41d915a20896aa21c6762
Alexey Sokolov <alexey@ebi.ac.uk>
2019-02-20 11:43:39 +0000

    Adding pubmedId fields

--- scripts/fetch_articles.py
+++ scripts/fetch_articles.py
@@ -5,15 +5,14 @@ import aiohttp
 import time
 
 # Global sets for ids
-ORGANISMS = set()
-SPECIMENS = set()
-DATASETS = set()
-FILES = set()
-ARTICLES = []
+ORGANISMS = {}
+SPECIMENS = {}
+DATASETS = {}
+FILES = {}
+ARTICLES = {}
 
 
-def main():
-    es = Elasticsearch([NODE1, NODE2])
+def main(es):
     organisms_id = retrieve_ids('organism', es)
     specimens_id = retrieve_ids('specimen', es)
     datasets_id = retrieve_ids('dataset', es)
@@ -48,9 +47,9 @@ async def fetch_all_articles(ids, my_type):
         tasks = []
         for my_id in ids:
             if my_type == 'check_specimen_for_organism':
-                task = asyncio.ensure_future(check_specimen_for_organism(session, my_id))
+                task = asyncio.ensure_future(check_specimen_for_organism(session, my_id, ids[my_id]))
             elif my_type == 'check_specimen_for_dataset':
-                task = asyncio.ensure_future(check_specimen_for_dataset(session, my_id))
+                task = asyncio.ensure_future(check_specimen_for_dataset(session, my_id, ids[my_id]))
             else:
                 task = asyncio.ensure_future(fetch_article(session, my_id, my_type))
             tasks.append(task)
@@ -63,83 +62,86 @@ async def fetch_article(session, my_id, my_type):
         results = await response.json()
         results = results['resultList']['result']
         if len(results) != 0:
+            articles_list = [result['pmcid'] for result in results]
             if my_type == 'organism':
-                ORGANISMS.add(my_id)
+                add_new_pair(ORGANISMS, my_id, articles_list)
 
                 # Add specimens
                 response = requests.get(
                     "http://data.faang.org/api/specimen/_search/?q=organism.biosampleId:{}&size=100000".format(my_id)
                 ).json()
                 for item in response['hits']['hits']:
-                    SPECIMENS.add(item['_id'])
+                    add_new_pair(SPECIMENS, item['_id'], articles_list)
 
                 # Add files
                 response = requests.get(
                     "http://data.faang.org/api/file/_search/?q=organism:{}&size=100000".format(my_id)).json()
                 for item in response['hits']['hits']:
-                    FILES.add(item['_id'])
+                    # FILES.add(item['_id'])
+                    add_new_pair(FILES, item['_id'], articles_list)
 
             elif my_type == 'specimen':
-                SPECIMENS.add(my_id)
+                add_new_pair(SPECIMENS, my_id, articles_list)
 
                 # Add organisms
                 response = requests.get("http://data.faang.org/api/specimen/{}".format(my_id)).json()
                 if 'biosampleId' in response['hits']['hits'][0]['_source']['organism']:
                     organism_id = response['hits']['hits'][0]['_source']['organism']['biosampleId']
-                    ORGANISMS.add(organism_id)
+                    add_new_pair(ORGANISMS, organism_id, articles_list)
 
                 # Add datasets
                 response = requests.get(
                     "http://data.faang.org/api/dataset/_search/?q=specimen.biosampleId:{}&size=100000".format(my_id)
                 ).json()
                 for item in response['hits']['hits']:
-                    DATASETS.add(item['_id'])
+                    add_new_pair(DATASETS, item['_id'], articles_list)
 
                 # Add files
                 response = requests.get(
                     "http://data.faang.org/api/file/_search/?q=specimen:{}&size=100000".format(my_id)).json()
                 for item in response['hits']['hits']:
-                    FILES.add(item['_id'])
+                    add_new_pair(FILES, item['_id'], articles_list)
 
             elif my_type == 'dataset':
-                DATASETS.add(my_id)
+                add_new_pair(DATASETS, my_id, articles_list)
 
                 # Add specimens
                 response = requests.get("http://data.faang.org/api/dataset/{}".format(my_id)).json()
                 for item in response['hits']['hits'][0]['_source']['specimen']:
-                    SPECIMENS.add(item['biosampleId'])
+                    add_new_pair(SPECIMENS, item['biosampleId'], articles_list)
 
                 # Add files
                 for item in response['hits']['hits'][0]['_source']['file']:
-                    FILES.add(item['fileId'])
+                    add_new_pair(FILES, item['fileId'], articles_list)
 
 
-async def check_specimen_for_organism(session, my_id):
+async def check_specimen_for_organism(session, my_id, articles_list):
     url = "http://data.faang.org/api/specimen/{}".format(my_id)
     async with session.get(url) as response:
         results = await response.json()
         if 'biosampleId' in results['hits']['hits'][0]['_source']['organism']:
             organism_id = results['hits']['hits'][0]['_source']['organism']['biosampleId']
-            ORGANISMS.add(organism_id)
+            add_new_pair(ORGANISMS, organism_id, articles_list)
 
 
-async def check_specimen_for_dataset(session, my_id):
+async def check_specimen_for_dataset(session, my_id, articles_list):
     url = "http://data.faang.org/api/dataset/_search/?q=specimen.biosampleId:{}&size=100000".format(my_id)
     async with session.get(url) as response:
         results = await response.json()
         for item in results['hits']['hits']:
-            DATASETS.add(item['_id'])
+            add_new_pair(DATASETS, item['_id'], articles_list)
 
 
-def update_records(records_array, array_type):
-    es = Elasticsearch(['wp-np3-e2', 'wp-np3-e3'])
-    body = {"doc": {"paperPublished": "true"}}
+def update_records(records_dict, array_type, es):
 
     print("Starting to update {} records:".format(array_type))
-    for index, item_id in enumerate(records_array):
+    for index, item_id in enumerate(records_dict):
         if index % 100 == 0:
-            ratio = round(index / len(records_array) * 100)
+            ratio = round(index / len(records_dict) * 100)
             print(f"{ratio} % is ready...")
+        body = {"doc": {"paperPublished": "true", "publishedArticles": [
+            {'pubmedId': pubmedId} for pubmedId in records_dict[item_id]]}}
+
         try:
             es.update(index=array_type, doc_type="_doc", id=item_id, body=body)
         except ValueError:
@@ -147,14 +149,22 @@ def update_records(records_array, array_type):
             continue
 
 
+def add_new_pair(target_dict, id_to_check, target_list):
+    if id_to_check not in target_dict:
+        target_dict[id_to_check] = set(target_list)
+    else:
+        target_dict[id_to_check].update(target_list)
+
+
 if __name__ == "__main__":
     start_time = time.time()
 
-    main()
-    update_records(SPECIMENS, 'specimen')
-    update_records(ORGANISMS, 'organism')
-    update_records(DATASETS, 'dataset')
-    update_records(FILES, 'file')
+    es = Elasticsearch(['wp-np3-e2', 'wp-np3-e3'])
+    main(es)
+    update_records(SPECIMENS, 'specimen', es)
+    update_records(ORGANISMS, 'organism', es)
+    update_records(DATASETS, 'dataset', es)
+    update_records(FILES, 'file', es)
 
     duration = time.time() - start_time
     print(f"Done in {round(duration / 60)} minutes")
