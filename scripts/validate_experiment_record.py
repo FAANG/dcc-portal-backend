0608478dae966792e5817a94f3139b2322f9bf6f
Alexey Sokolov <alexey@ebi.ac.uk>
2019-03-08 14:33:20 +0000

    Rewriting validate_experiment_record

--- scripts/validate_experiment_record.py
+++ scripts/validate_experiment_record.py
@@ -1,3 +1,6 @@
+import os
+
+
 def validate_total_experiment_records(target_dict, rulesets):
     total_results = dict()
     portion_size = 600
@@ -25,5 +28,39 @@ def get_validation_results(part, my_type, rulesets):
         total_results = merge_results(total_results, validation_results, ruleset)
     return total_results
 
-def validate_record(part, my_type, ruleset):
+
+def validate_record(data, ruleset):
+    tmp_out_file = 'tmp_experiment_records.json'
+    with open(tmp_out_file, 'w') as w:
+        w.write("[\n")
+        for index, item in enumerate(data):
+            converted_data = convert_data(item)
+            converted_data = json.dumps(converted_data)
+            if index != 0:
+                w.write(",\n")
+            w.write(f"{converted_data}\n")
+        w.write("]\n")
+    try:
+        command = f'curl -F "format=json" -F "rule_set_name={ruleset}" -F "file_format=JSON"' + \
+                  f' -F "metadata_file=@{tmp_out_file}" "https://www.ebi.ac.uk/vg/faang/validate" > validation.json'
+        os.system(command)
+    except:
+        # TODO log to error
+        print("Validation Error!!!")
+        sys.exit(0)
+    with open('validation.json', 'r') as f:
+        data = json.load(f)
+    return parse_validation_results(data['entities'])
+
+
+def merge_results(total_results, validation_results, ruleset):
     pass
+
+
+def convert_data(item):
+    pass
+
+
+def parse_validation_results(data):
+    pass
+
