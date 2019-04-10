8abca9b75cbf6dd4cfe0733c12fed7b9fa5d7fca
Alexey Sokolov <alexey@ebi.ac.uk>
2019-04-10 10:39:31 +0100

    Writing parse script for organism summary

--- scripts/create_summary.py
+++ scripts/create_summary.py
@@ -1,4 +1,5 @@
 from elasticsearch import Elasticsearch
+import requests
 
 from utils import *
 
@@ -18,7 +19,85 @@ class CreateSummary:
         self.logger_instance = logger_instance
 
     def create_organism_summary(self):
-        pass
+        results = requests.get("http://test.faang.org/api/organism/_search/?size=100000").json()
+        standard_data = dict()
+        sex_data = dict()
+        paper_published_data = {'yes': 0, 'no': 0}
+        organism_data = dict()
+        breeds_data = dict()
+        for item in results['hits']['hits']:
+            # Get data for standard_data
+            standard_data.setdefault(item['_source']['standardMet'], 0)
+            standard_data[item['_source']['standardMet']] += 1
+
+            # get data for sex_data
+            sex = item['_source']['sex']['text']
+            if sex in MALES:
+                sex = 'male'
+            elif sex in FEMALES:
+                sex = 'female'
+            else:
+                sex = 'not determined'
+            sex_data.setdefault(sex, 0)
+            sex_data[sex] += 1
+
+            # get data for paper_published_data
+            if 'paperPublished' in item['_source']:
+                paper_published_data['yes'] += 1
+            else:
+                paper_published_data['no'] += 1
+
+            # get data for organism_data
+            organism = item['_source']['organism']['text']
+            organism_data.setdefault(organism, 0)
+            organism_data[organism] += 1
+
+            # get data for breeds_data
+            organism = item['_source']['organism']['text']
+            breed = item['_source']['breed']['text']
+            breeds_data.setdefault(organism, {})
+            breeds_data[organism].setdefault(breed, 0)
+            breeds_data[organism][breed] += 1
+        # create document for es
+        results = dict()
+        for k, v in sex_data.items():
+            results.setdefault('sexSummary', [])
+            results['sexSummary'].append({
+                'name': k,
+                'value': v
+            })
+        for k, v in paper_published_data.items():
+            results.setdefault('paperPublishedSummary', [])
+            results['paperPublishedSummary'].append({
+                'name': k,
+                'value': v
+            })
+        for k, v in standard_data.items():
+            results.setdefault('standardSummary', [])
+            results['standardSummary'].append({
+                'name': k,
+                'value': v
+            })
+        for k, v in organism_data.items():
+            results.setdefault('organismSummary', [])
+            results['organismSummary'].append({
+                'name': k,
+                'value': v
+            })
+        for k, v in breeds_data.items():
+            results.setdefault('breedSummary', [])
+            tmp_list = list()
+            for tmp_k, tmp_v in v.items():
+                tmp_list.append({
+                    'name': tmp_k,
+                    'value': tmp_v
+                })
+            results['breedSummary'].append({
+                "name": k,
+                "value": tmp_list
+            })
+        body = {"doc": results}
+        self.es_instance.update(index="summary_organism", doc_type="_doc", id="summary_organism", body=body)
 
     def create_specimen_summary(self):
         pass
