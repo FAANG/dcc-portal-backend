d9a0333d9a12eec864506c63488d634bd1585615
Jun Fan <junf@ebi.ac.uk>
2019-04-15 12:42:15 +0100

    PR comment: improve documentation and replace dict with set

--- scripts/compare_records_in_two_indices.py
+++ scripts/compare_records_in_two_indices.py
@@ -4,7 +4,7 @@ compare two versions of same type records stored in the two different indices
 import click
 import constants
 import requests
-from typing import Dict
+from typing import Set
 
 
 @click.command()
@@ -28,10 +28,10 @@ from typing import Dict
 def main(es_host, es_index_1, es_index_2, es_type):
     """
     The main function
-    :param es_host:
-    :param es_index_1:
-    :param es_index_2:
-    :param es_type:
+    :param es_host: elastic search host server
+    :param es_index_1: the index prefix 1
+    :param es_index_2: the index prefix 2
+    :param es_type: the type of records to be compared
     :return:
     """
     error_flag = False
@@ -51,31 +51,31 @@ def main(es_host, es_index_1, es_index_2, es_type):
     if error_flag:
         exit()
 
-    url_1: str = f"{es_host}/{es_index_1}_{es_type}/_search?_source=_id&size=10000"
-    url_2: str = f"{es_host}/{es_index_2}_{es_type}/_search?_source=_id&size=10000"
+    url_1: str = f"{es_host}/{es_index_1}_{es_type}/_search?_source=_id&size=100000"
+    url_2: str = f"{es_host}/{es_index_2}_{es_type}/_search?_source=_id&size=100000"
     resp1 = get_ids(url_1)
     resp2 = get_ids(url_2)
-    for record_id in sorted(resp1.keys()):
+    for record_id in sorted(resp1):
         if record_id in resp2:
-            del resp2[record_id]
+            resp2.remove(record_id)
         else:
             print(f"Only in {es_index_1}_{es_type}: {record_id}")
 
     if resp2:
-        for record_id in sorted(resp2.keys()):
+        for record_id in sorted(resp2):
             print(f"Only in {es_index_2}_{es_type}: {record_id}")
 
 
-def get_ids(url: str) -> Dict[str, int]:
+def get_ids(url: str) -> Set[str]:
     """
     Return the id list in the form of Dict
     :param url: used to be curled
     :return: the id list
     """
     response = requests.get(url).json()
-    results = dict()
+    results = set()
     for hit in response['hits']['hits']:
-        results[hit['_id']] = 1
+        results.add(hit['_id'])
     return results
 
 
