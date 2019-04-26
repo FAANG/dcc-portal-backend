39808db2353626e5180a27f11cb22f6fba5d021d
Alexey Sokolov <alexey@ebi.ac.uk>
2019-04-26 15:27:28 +0100

    Adding unit test for create experiment protocol

--- scripts/test_create_protocols.py
+++ scripts/test_create_protocols.py
@@ -54,3 +54,85 @@ class TestCreateProtocols(unittest.TestCase):
                    'ROSLIN_SOP_Harvest_of_Large_Animal_Tissues_20160516.pdf',
             'protocolType': 'samples'}, doc_type='_doc', id='ROSLIN_SOP_Harvest_of_Large_Animal_Tissues_20160516.pdf',
             index='protocol_samples')
+
+    def test_create_experiment_protocol(self):
+        inner_value = {'experimentTarget': 'polyA RNA', 'experimentalProtocol': {
+            'filename': 'truseq-stranded-mrna-sample-prep-guide-15031047-e.pdf',
+            'url': 'https://support.illumina.com/content/dam/illumina-support/documents/documentation/'
+                   'chemistry_documentation/samplepreps_truseq/truseqstrandedmrna/'
+                   'truseq-stranded-mrna-sample-prep-guide-15031047-e.pdf'}, 'sequencingLocationLatitude': {
+            'unit': 'decimal degrees', 'text': '55.923954'}, 'extractionProtocol': {
+            'filename': 'ROSLIN_SOP_Isolation_of_RNA_from__preserved_tissue_20161108.pdf',
+            'url': 'ftp://ftp.faang.ebi.ac.uk/ftp/protocols/samples/'
+                   'ROSLIN_SOP_Isolation_of_RNA_from__preserved_tissue_20161108.pdf'},
+                       'sequencingLocationLongitude': {'unit': 'decimal degrees', 'text': '-3.173114'},
+                       'sampleStorageProcessing': 'cryopreservation, other', 'sampleStorage': 'RNAlater, frozen',
+                       'libraryPreparationLocation': 'Edinburgh Genomics, The University of Edinburgh, EH9 3FL, '
+                                                     'Edinburgh, Scotland', 'assayType': 'transcription profiling by '
+                                                                                         'high throughput sequencing',
+                       'standardMet': 'FAANG', 'libraryPreparationLocationLongitude': {'unit': 'decimal degrees',
+                                                                                       'text': '-3.173114'},
+                       'libraryPreparationDate': {'unit': '', 'text': 'not provided'},
+                       'sequencingLocation': 'Edinburgh Genomics, The University of Edinburgh, EH9 3FL, Edinburgh, '
+                                             'Scotland', 'libraryPreparationLocationLatitude': {
+                'unit': 'decimal degrees', 'text': '55.923954'}, 'versionLastStandardMet': '3.6',
+                       'accession': 'ERX2403514', 'samplingToPreparationInterval': {'unit': 'days', 'text': '120.0'},
+                       'RNA-seq': {'rnaPurity260280ratio': '2.04', 'rnaPreparation3AdapterLigationProtocol': {
+                           'filename': 'truseq-stranded-mrna-sample-prep-guide-15031047-e.pdf',
+                           'url': 'https://support.illumina.com/content/dam/illumina-support/documents/'
+                                  'documentation/chemistry_documentation/samplepreps_truseq/truseqstrandedmrna/'
+                                  'truseq-stranded-mrna-sample-prep-guide-15031047-e.pdf'},
+                                   'libraryGenerationPcrProductIsolationProtocol': {
+                                       'filename': 'truseq-stranded-mrna-sample-prep-guide-15031047-e.pdf',
+                                       'url': 'https://support.illumina.com/content/dam/illumina-support/'
+                                              'documents/documentation/chemistry_documentation/samplepreps_truseq/'
+                                              'truseqstrandedmrna/truseq-stranded-mrna-sample-prep-guide-15031047-e.pdf'
+                                   }, 'readStrand': 'mate 2 sense', 'rnaIntegrityNumber': '8.5',
+                                   'rnaPurity260230ratio': '2.19', 'libraryGenerationProtocol': {
+                               'filename': 'truseq-stranded-mrna-sample-prep-guide-15031047-e.pdf',
+                               'url': 'https://support.illumina.com/content/dam/illumina-support/documents/'
+                                      'documentation/chemistry_documentation/samplepreps_truseq/truseqstrandedmrna/'
+                                      'truseq-stranded-mrna-sample-prep-guide-15031047-e.pdf'},
+                                   'rnaPreparation5AdapterLigationProtocol': {
+                                       'filename': 'truseq-stranded-mrna-sample-prep-guide-15031047-e.pdf',
+                                       'url': 'https://support.illumina.com/content/dam/illumina-support/documents/'
+                                              'documentation/chemistry_documentation/samplepreps_truseq/'
+                                              'truseqstrandedmrna/truseq-stranded-mrna-sample-prep-guide-15031047-e.pdf'
+                                   }, 'preparationReverseTranscriptionProtocol': {
+                               'filename': 'truseq-stranded-mrna-sample-prep-guide-15031047-e.pdf',
+                               'url': 'https://support.illumina.com/content/dam/illumina-support/documents/'
+                                      'documentation/chemistry_documentation/samplepreps_truseq/truseqstrandedmrna/'
+                                      'truseq-stranded-mrna-sample-prep-guide-15031047-e.pdf'}},
+                       'sequencingDate': {'unit': '', 'text': 'not provided'}}
+        return_value = {
+            'hits': {
+                'hits': [
+                    {
+                        '_source': inner_value
+                    }
+                ]
+            }
+        }
+        es_staging = Mock()
+        es_staging.search.return_value = return_value
+        logger = Mock()
+        test_object = create_protocols.CreateProtocols(es_staging, logger)
+        test_object.create_experiment_protocol()
+        self.assertEqual(es_staging.search.call_count, 1)
+        es_staging.index.assert_called_with(body={'name': 'preparationReverseTranscriptionProtocol',
+                                                  'experimentTarget': 'polyA RNA',
+                                                  'assayType': 'transcription profiling by high throughput sequencing',
+                                                  'key': 'preparationReverseTranscriptionProtocol-'
+                                                         'transcriptionprofilingbyhighthroughputsequencing-polyARNA',
+                                                  'url': 'https://support.illumina.com/content/dam/illumina-support/'
+                                                         'documents/documentation/chemistry_documentation/'
+                                                         'samplepreps_truseq/truseqstrandedmrna/'
+                                                         'truseq-stranded-mrna-sample-prep-guide-15031047-e.pdf',
+                                                  'filename': 'truseq-stranded-mrna-sample-prep-guide-15031047-e.pdf',
+                                                  'experiments': [{'accession': 'ERX2403514',
+                                                                   'sampleStorage': 'RNAlater, frozen',
+                                                                   'sampleStorageProcessing':
+                                                                       'cryopreservation, other'}]},
+                                            doc_type='_doc', id='preparationReverseTranscriptionProtocol'
+                                                                '-transcriptionprofilingbyhighthroughputsequencing'
+                                                                '-polyARNA', index='protocol_files')
