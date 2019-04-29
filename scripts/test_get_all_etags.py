fce750a6185ce396804078f8f62abe1c9ede33b0
Alexey Sokolov <alexey@ebi.ac.uk>
2019-04-29 15:24:17 +0100

    Writing unit test for get_all_etags

---
+++ scripts/test_get_all_etags.py
@@ -0,0 +1,15 @@
+"""
+Test cases for get_all_etags
+"""
+import unittest
+from unittest.mock import Mock
+from unittest.mock import patch
+
+import get_all_etags
+
+
+class TestGetAllEtags(unittest.TestCase):
+    def test_fetch_biosample_ids(self):
+        with patch('get_all_etags.requests') as mock_requests:
+            get_all_etags.fetch_biosample_ids()
+            self.assertEqual(mock_requests.get.call_count, 1)
