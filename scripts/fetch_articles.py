176291f31b0d9496d5fb532c9b6f684ba1b4590e
Jun Fan <junf@ebi.ac.uk>
2020-01-08 12:56:33 +0000

    add some datasets details to Elasticsearch to make it possible to do related dataset table in publication detail page

--- scripts/fetch_articles.py
+++ scripts/fetch_articles.py
@@ -1,10 +1,20 @@
+"""
+The script is based on the API provided by europe PMC http://europepmc.org/
+ePMC treats the full text as free text, so when the accessions are mentioned in the full text, they will be indexed.
+Due to the factor that it is much more likely to mention the dataset accession as a whole than each individual records,
+e.g. paper PMC6500009 mentions several dataset accessions PRJNA526824 etc and the sample range SAMN11119414-SAMN11119461
+After retrieving dataset-article relationships, the same relationships will be extended to all samples/files under the
+dataset
+Using the example above, each individual sample between SAMN11119414-SAMN11119461 will have the article PMC6500009
+"""
 import requests
 from elasticsearch import Elasticsearch
 import click
-from utils import create_logging_instance, remove_underscore_from_end_prefix, get_record_ids, insert_into_es
-from constants import STAGING_NODE1, DEFAULT_PREFIX
+from utils import create_logging_instance, remove_underscore_from_end_prefix, get_record_ids, get_record_details, \
+    insert_into_es
+from constants import STAGING_NODE1, DEFAULT_PREFIX, STANDARD_FAANG
 from typing import Dict, Set
-import pprint
+
 logger = create_logging_instance('fetch_articles', to_file=False)
 
 
@@ -28,13 +38,20 @@ def main(es_hosts, es_index_prefix):
 
     es_index_prefix = remove_underscore_from_end_prefix(es_index_prefix)
     logger.info(f"Index: {es_index_prefix}_article")
-    datasets = get_record_ids(hosts[0], es_index_prefix, 'dataset', only_faang=False)
+    datasets = get_record_details(hosts[0], es_index_prefix, 'dataset', ['standardMet', 'species'])
     existing_articles = get_record_ids(hosts[0], es_index_prefix, 'article', only_faang=False)
     logger.info("The number of existing datasets: " + str(len(datasets)))
     logger.info("The number of existing articles: " + str(len(existing_articles)))
     article_details = dict()
     article_datasets: Dict[str, Set] = dict()
-    for dataset_id in datasets:
+    dataset_count = 0
+    # for all datasets existing in the Elastic search, search for the publications based on the dataset accession
+    for dataset_id in datasets.keys():
+        # logging progress, not related to the main algorithm
+        dataset_count = dataset_count + 1
+        if dataset_count % 200 == 0:
+            logger.info(f'Processed {dataset_count} datasets')
+        # get dataset related publication using europe PMC search API
         url = f"https://www.ebi.ac.uk/europepmc/webservices/rest/search?query={dataset_id}&format=json"
         epmc_result = requests.get(url).json()
         hit_count = epmc_result['hitCount']
@@ -46,6 +63,9 @@ def main(es_hosts, es_index_prefix):
                     continue
                 if 'source' in hit and hit['source'] == 'PPR':
                     continue
+                # determine the article id which will be used in ES, PMC id is preferred, because
+                # 1) it has PMC prefix rather than a string of digits
+                # 2) PMC guarantees open access, more likely to have dataset accession linked
                 article_id = determine_article_id(hit)
                 if len(article_id) == 0:
                     logger.error(f"Study {dataset_id} has related article without Identifier")
@@ -69,12 +89,25 @@ def main(es_hosts, es_index_prefix):
                 article_datasets.setdefault(article_id, set())
                 article_datasets[article_id].add(dataset_id)
 
-    logger.info(f'Retrieved {len(article_details.keys())} articles')
+    logger.info(f'Retrieved {len(article_details.keys())} articles from all datasets')
     es = Elasticsearch(hosts)
     for article_id in article_details.keys():
         es_article = article_details[article_id]
-        datasets = article_datasets[article_id]
-        es_article['relatedDatasets'] = list(datasets)
+        all_faang_datasets_flag = 'FAANG only'
+        es_datasets = list()
+        for dataset_id in article_datasets[article_id]:
+            tmp = dict()
+            tmp['accession'] = dataset_id
+            tmp['standardMet'] = datasets[dataset_id]['standardMet']
+            if tmp['standardMet'] != STANDARD_FAANG:
+                all_faang_datasets_flag = "All"
+            species = list()
+            for tmp_sp in datasets[dataset_id]['species']:
+                species.append(tmp_sp['text'])
+            tmp['species'] = species
+            es_datasets.append(tmp)
+        es_article['relatedDatasets'] = es_datasets
+        es_article['datasetSource'] = all_faang_datasets_flag
         insert_into_es(es, es_index_prefix, 'article', article_id, es_article)
         if article_id in existing_articles:
             existing_articles.remove(article_id)
@@ -86,19 +119,27 @@ def main(es_hosts, es_index_prefix):
 
 
 def parse_field(es_article, hit, es_key, api_key):
+    """
+    update the record with the value in the hit, if the key not found, then the record does not change
+    :param es_article: the record data which will be inserted into ElasticSearch
+    :param hit: the input data
+    :param es_key: the key in the elastic search record
+    :param api_key: the key to search the input data
+    :return: the updated record
+    """
     if api_key in hit:
         es_article[es_key] = hit[api_key]
     return es_article
 
 
 def determine_article_id(epmc_hit):
-    '''
+    """
     Determine the article id to be used in ElasticSearch from the ePMC record
     The priority is pmcid (Pubmed Central id which provides open access), pmid (pubmed id), doi and id used by ePMC
     If none of those values is available, return empty string, a sign of error to be dealt with in the caller
     :param epmc_hit: the ePMC record from their API
     :return: the determined id
-    '''
+    """
     if 'pmcid' in epmc_hit:
         return epmc_hit['pmcid']
     elif 'pmid' in epmc_hit:
--- scripts/utils.py
+++ scripts/utils.py
@@ -6,6 +6,7 @@ import logging
 from typing import Set, List, Dict
 import requests
 from constants import STANDARDS, STANDARD_FAANG, TYPES
+from elasticsearch import Elasticsearch
 from misc import convert_readable
 
 
@@ -76,22 +77,57 @@ def get_record_ids(host: str, es_index_prefix: str, data_type: str, only_faang=T
     :param only_faang: indiciates whether only include FAANG standard records (when True) or all records (when False)
     :return: set of FAANG dataset id
     """
-    if data_type not in TYPES:
+    standard_field = 'standardMet'
+    if data_type == 'article':
+        standard_field = ''
+    details = get_record_details(host, es_index_prefix, data_type, [standard_field])
+    if len(details) == 0:
         return set()
+    if only_faang:
+        results = set()
+        for hit in details.keys():
+            if details[hit]['standard'] == STANDARD_FAANG:
+                results.add(hit)
+        return results
+    else:
+        return set(details.keys())
+
+
+def get_record_number(host: str, es_index_prefix: str, data_type: str) -> int:
+    """
+    Get the number of records of one type in the Elasticsearch, which is necessary to do a full list retrieval as
+    the default size is 20 and an arbitrary hard-coded value is also not ideal, may become too small one day
+    :param host: the Elastic Search server address
+    :param es_index_prefix: the Elastic Search dataset index
+    :param data_type: the type of records
+    :return: the number of records
+    """
+    if data_type not in TYPES:
+        return 0
     url = f"http://{host}/{es_index_prefix}_{data_type}/_search?_source=standardMet"
     response = requests.get(url).json()
-    total_number = response['hits']['total']
+    return response['hits']['total']
+
+
+def get_record_details(host: str, es_index_prefix: str, data_type: str, return_fields: List) -> Dict:
+    """
+    Get the subset of record details
+    :param host: the Elastic Search server address
+    :param es_index_prefix: the Elastic Search dataset index
+    :param data_type: the type of records
+    :param return_fields: the list of fields containing the wanted information
+    :return: a dict having record id as keys, and all field values as values
+    """
+    total_number = get_record_number(host, es_index_prefix, data_type)
     if total_number == 0:
-        return set()
-    results = set()
-    url = f"{url}&size={total_number}"
-    response = requests.get(url).json()
-    for hit in response['hits']['hits']:
-        if only_faang:
-            if 'standardMet' in  hit['_source'] and hit['_source']['standardMet'] == STANDARD_FAANG:
-                results.add(hit['_id'])
-        else:
-            results.add(hit['_id'])
+        return dict()
+    es = Elasticsearch(host)
+    index_name = f'{es_index_prefix}_{data_type}'
+    source_str = ','.join(return_fields)
+    data = es.search(index=index_name, size=total_number, _source=source_str)
+    results = dict()
+    for hit in data['hits']['hits']:
+        results[hit['_id']] = hit['_source']
     return results
 
 
