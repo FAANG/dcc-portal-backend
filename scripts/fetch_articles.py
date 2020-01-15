bb5b6377a33b6a9f6655cfbe9ca043c173175b66
Jun Fan <junf@ebi.ac.uk>
2020-01-15 01:15:37 +0000

    update article info in organism, specimen, file Elastic search indices

--- scripts/fetch_articles.py
+++ scripts/fetch_articles.py
@@ -38,15 +38,26 @@ def main(es_hosts, es_index_prefix):
 
     es_index_prefix = remove_underscore_from_end_prefix(es_index_prefix)
     logger.info(f"Index: {es_index_prefix}_article")
-    datasets = get_record_details(hosts[0], es_index_prefix, 'dataset', ['standardMet', 'species'])
+    # get existing dataset (to work out articles in file and specimen and existing specimen to calculate organism
+    datasets = get_record_details(hosts[0], es_index_prefix, 'dataset',
+                                  ['standardMet', 'species', 'specimen.biosampleId', 'file.fileId'])
+    specimens = get_record_details(hosts[0], es_index_prefix, 'specimen', ['organism.biosampleId'])
+    # get existing articles
     existing_articles = get_record_ids(hosts[0], es_index_prefix, 'article', only_faang=False)
     logger.info("The number of existing datasets: " + str(len(datasets)))
     logger.info("The number of existing articles: " + str(len(existing_articles)))
+    # detailed article information which is used to insert into article ES index, keys are article id
     article_details = dict()
+    # basic article information which is used in related records, e.g. specimen, dataset etc, keys are article id
+    article_basics = dict()
+    # record article dataset relationship, keys are article id
     article_datasets: Dict[str, Set] = dict()
+    # for progress purpose
     dataset_count = 0
+    # one dataset could have multiple articles, keys are dataset id, same naming pattern for other record type
     article_for_datasets: Dict[str, Set] = dict()
-    # for all datasets existing in the Elastic search, search for the publications based on the dataset accession
+
+    # for all datasets existing in the Elasticsearch, search for the publications based on the dataset accession
     for dataset_id in datasets.keys():
         # logging progress, not related to the main algorithm
         dataset_count = dataset_count + 1
@@ -86,6 +97,13 @@ def main(es_hosts, es_index_prefix):
                     es_article = parse_field(es_article, hit, 'pages', 'pageInfo')
                     es_article = parse_field(es_article, hit, 'isOpenAccess', 'isOpenAccess')
                     article_details[article_id] = es_article
+                    article_basic_info = dict()
+                    # the article information displayed in other entities
+                    article_basic_info['articleId'] = article_id
+                    article_basic_info = parse_field(article_basic_info, hit, 'title', 'title')
+                    article_basic_info = parse_field(article_basic_info, hit, 'journal', 'journalTitle')
+                    article_basic_info = parse_field(article_basic_info, hit, 'year', 'pubYear')
+                    article_basics[article_id] = article_basic_info
 
                 article_datasets.setdefault(article_id, set())
                 article_datasets[article_id].add(dataset_id)
@@ -93,7 +111,9 @@ def main(es_hosts, es_index_prefix):
                 article_for_datasets[dataset_id].add(article_id)
 
     logger.info(f'Retrieved {len(article_details.keys())} articles from all datasets')
+
     es = Elasticsearch(hosts)
+    # deal with article index
     for article_id in article_details.keys():
         es_article = article_details[article_id]
         all_faang_datasets_flag = 'FAANG only'
@@ -118,16 +138,75 @@ def main(es_hosts, es_index_prefix):
     for not_needed_article_id in existing_articles:
         es.delete(index=f'{es_index_prefix}_article', doc_type="_doc", id=not_needed_article_id)
 
-    for dataset_id in article_for_datasets.keys():
+    logger.info("Update articles within dataset index")
+    update_article_info(article_basics, article_for_datasets, es, es_index_prefix, 'dataset')
+    logger.info("Update articles within specimen index")
+    # update specimen, 'specimen' 'biosampleId' are referenced to the parameters used in datasets = get_record_details
+    article_for_specimens: Dict[str, Set] = extract_article_from_related_entity(datasets, article_for_datasets,
+                                                                'specimen', 'biosampleId')
+    update_article_info(article_basics, article_for_specimens, es, es_index_prefix, 'specimen')
+
+    logger.info("Update articles within file index")
+    article_for_files: Dict[str, Set] = extract_article_from_related_entity(datasets, article_for_datasets, 'file', 'fileId')
+    update_article_info(article_basics, article_for_files, es, es_index_prefix, 'file')
+
+    logger.info("Update articles within organism index")
+    article_for_organisms: Dict[str, Set] = extract_article_from_related_entity(specimens, article_for_specimens,
+                                                                'organism', 'biosampleId')
+    update_article_info(article_basics, article_for_organisms, es, es_index_prefix, 'organism')
+
+    logger.info("Finishing importing article")
+
+
+def extract_article_from_related_entity(source_data, source_article_data,
+                                        relationship_key, relationship_secondary_key=''):
+    """
+    Different types of records are linked, e.g. specimen to organism
+    This function assigns the article information from the related record to the target record ,
+    e.g. article for specimen (target) from dataset (source / related)
+    :param source_data: the search result from ES for the related record
+    :param source_article_data: the article information for the related record
+    :param relationship_key: the ES field/section name
+    :param relationship_secondary_key: the field name in the section if needed,
+    e.g. biosampleId in organism section in specimen ES
+    :return: articles for the target record
+    """
+    result: Dict[str, Set] = dict()
+    for source_id in source_data.keys():
+        # only deal with source data having articles
+        if source_id in source_article_data and source_id in source_data and relationship_key in source_data[source_id]:
+            # unify to be list, in ES, it is only defined as keyword, could be list or string, files in dataset is list
+            # while organism in specimen is a string
+            tmp = list()
+            if type(source_data[source_id][relationship_key]) == list:
+                tmp = source_data[source_id][relationship_key]
+            else:
+                tmp.append(source_data[source_id][relationship_key])
+            for record in tmp:
+                if len(relationship_secondary_key) == 0:
+                    target_id = record
+                else:
+                    target_id = record[relationship_secondary_key]
+                result.setdefault(target_id, set())
+                for article_id in source_article_data[source_id]:
+                    result[target_id].add(article_id)
+    return result
+
+
+def update_article_info(article_basics, article_for_others, es, es_index_prefix, record_type) -> None:
+    """
+    update article information in other Elasticsearch indices
+    :param article_basics: the collection of article information which is displayed in the other type record detail page
+    :param article_for_others: the list of records need to be updated with the articles
+    :param es: Elastic search instance
+    :param es_index_prefix: the Elastic search index prefix
+    :param record_type: the type of the record
+    :return:
+    """
+    for record_id in article_for_others.keys():
         publications: List = list()
-        for article_id in article_for_datasets[dataset_id]:
-            article_detail = article_details[article_id]
-            tmp = dict()
-            tmp['articleId'] = article_id
-            tmp['title'] = article_detail['title']
-            tmp['journal'] = article_detail['journal']
-            tmp['year'] = article_detail['year']
-            publications.append(tmp)
+        for article_id in article_for_others[record_id]:
+            publications.append(article_basics[article_id])
         body = {
             "doc":
                 {"paperPublished": "true",
@@ -135,13 +214,10 @@ def main(es_hosts, es_index_prefix):
                  }
         }
         try:
-            es.update(index=f'{es_index_prefix}_dataset', doc_type="_doc", id=dataset_id, body=body)
+            es.update(index=f'{es_index_prefix}_{record_type}', doc_type="_doc", id=record_id, body=body)
         except ValueError:
-            print("ValueError {}".format(dataset_id))
+            print(f"Update goes wrong {record_id} in {record_type}")
             continue
-
-
-    logger.info("Finishing importing article")
 
 
 def parse_field(es_article, hit, es_key, api_key):
